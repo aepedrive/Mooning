123d0785c7aa57864f0d5ee8858adb16137463f4
aepedrive <armando.pedraza1979@gmail.com>
2013-07-03 19:13:54 -0600

    Added macros for genetic algorithm

--- Mooning/CLander.h
+++ Mooning/CLander.h
@@ -1,16 +1,6 @@
 #ifndef CLANDER_H
 #define CLANDER_H
 
-//-----------------------------------------------------------------------
-//
-//  Name: CLander.h
-//
-//  Author: Mat Buckland 2002
-//
-//	Desc: A class to define a lunar lander object
-//
-//-----------------------------------------------------------------------
-
 #include "windows.h"
 #include <vector>
 
@@ -21,11 +11,63 @@
 #include "C2DMatrix.h"
 
 using namespace std;
+/*! \file CLander.h
+	\brief Header file. struct SGene and class CLander definitions.
+*/
 
+//! Enumerate a type for each different action the Lander can perform
+/*! Defines actions taken by spaceship */
+enum action_type{rotate_left, /*!< Rotate counterclockwise */
+				 rotate_right,/*!< Rotate clockwise */
+				 thrust,	  /*!< Apply rocket thrust */
+				 non/*!< Do nothing */};
+
+//-------------------------------------------------------------------
+struct SGene
+{
+	action_type action;
+	
+	int duration; /*!< duration the action is applied measured in ticks */
+
+	SGene()
+	{
+		/*! \brief
+		* Creates a random action of a random duration
+		*/
+		action = (action_type)RandInt(0, 3);
+		duration = RandInt(1, MAX_ACTION_DURATION);
+	}
+
+	SGene(action_type a , int d):action(a), duration(d) {}
+
+	//need to overload the == operator so we can test if actions are 
+	//equal (used in the crossover process of the GA)
+	bool operator==(const SGene &rhs) const
+	{
+		return (action == rhs.action) && (duration == rhs.duration);
+	}
+};
+
+//-------------------------------------------------------------------
 
 class CLander
 {
 private:
+		
+	SVector2D		m_vStartPos; /*!< keep a record of the start position */
+	
+	double			m_dStartRotation; /*!< keep a record of the start rotation */
+	
+	int         m_iPadX; /*!< this will hold the horizontal position of the landing pad */
+	
+	vector<action_type> m_vecActions; /*!< This will hold a series of actions decoded from the ships chromosomes */
+	
+		
+	int         m_cTick; /*!< The action counter. Tells us which is the current action. */
+	//its fitness score
+	double			m_dFitness;
+
+	void CalculateFitness();
 	
 	//position in world
 	SVector2D		        m_vPos;
@@ -82,23 +124,29 @@ private:
 
 public:
 
-	CLander(int       cxClient, //so we can keep a local record 
-          int       cyClient, //of the window dimensions
-          double    rot,      //starting rotation of lander
-          SVector2D pos,      //starting position of lander
-          SVector2D pad);     //landing pad position
+	CLander(int cxClient,	/*!< [in] Local record of Window's x position*/
+          int cyClient,		/*!< [in] Local record of Window's y position*/
+          double rot,		/*!< [in] Starting rotation of lander */
+          SVector2D pos,    /*!< [in] Starting position of lander */
+          SVector2D pad /*!< [in] Landing pad position */);
   
-	void Render(HDC &surface);
+	void Render(HDC &surface); /*!< Renders ship */
+  
+	void Reset(SVector2D &NewPadPos); /*!< Resets all relevant variables for the start of a new attempt */
 
-  //resets all relevant variables for the start of a new attempt
-  void Reset(SVector2D &NewPadPos);
+	void  UpdateShip(double TimeElapsed); /*!< Updates the ship from a keypress */
 
-  //updates the ship from a keypress
-  void  UpdateShip(double TimeElapsed);
+	bool UpdateShip(); /*!< Another implemetation of UpdateShip. Will know its function
+						later on 2103-07-03. */
+	void Decode(const vector<SGene> &actions); /*!< Will know its function later on. 2013-07-03. */
+
+	/// Accesor method
+	SVector2D Pos()const {return m_vPos;}
+	/// Accesor method
+	vector<action_type> GetActionVector() const {return m_vecActions;}
+	/// Accesor method
+	double Fitness() const {return m_dFitness;}
 
 };
-	
-	
-
 
 #endif
--- Mooning/defines.h
+++ Mooning/defines.h
@@ -1,25 +1,33 @@
 #ifndef DEFINES_H
 #define DEFINES_H
-
+/*! \file defines.h
+	\brief In addition to the constants used in Manned Landing
+			defines constants for genetic algorithm
+*/
 
 
 //------------------------------------------------------------------
 //
 //	general defines
 //------------------------------------------------------------------
-#define WINDOW_WIDTH				400	
-#define WINDOW_HEIGHT				400
+#define WINDOW_WIDTH		400	
+#define WINDOW_HEIGHT		400
 
-#define FRAMES_PER_SECOND		60
+#define FRAMES_PER_SECOND	60
 
-#define	PI						      3.14159265358979
-#define HALF_PI				      PI/2
-#define TWO_PI			      	PI*2
-
+#define	PI					3.14159265358979
+#define HALF_PI				PI/2
+#define TWO_PI			    PI*2
+#define BIG_NUMBER          9999999
 //------------------------------------------------------------------
 //
 //	defines used in CLander
 //------------------------------------------------------------------
+
+//maximim amount of frames an action may be undertaken
+#define MAX_ACTION_DURATION   30
+
+
 #define SHIPS_MASS            100
 
 #define THRUST_PER_SECOND     350.0
@@ -27,6 +35,10 @@
 
 //approx gravity on moon
 #define GRAVITY               -1.63
+
+#define GRAVITY_PER_TICK      GRAVITY/FRAMES_PER_SECOND
+#define THRUST_PER_TICK       THRUST/FRAMES_PER_SECOND
+#define ROTATION_PER_TICK     ROTATION/FRAMES_PER_SECOND
 
 #define LANDER_SCALE          10
 
@@ -46,7 +58,28 @@
 //the ship must be to satisfy the landing criteria
 #define DIST_TOLERANCE         10
 
+//--------------------------------------------------------------------
+//	
+//	defines used in the genetic algorithm
+//--------------------------------------------------------------------
+#define POP_SIZE              100
+#define CHROMO_LENGTH         30
 
+#define MUTATION_RATE				  0.01
+#define CROSSOVER_RATE				0.7
+
+//used for elitism
+#define NUM_ELITE             4
+#define NUM_COPIES_ELITE      1
+//this is the maximum amount of time the duration
+//of an action can be altered by the mutation operator
+#define MAX_MUTATION_DURATION MAX_ACTION_DURATION/2
+//number of genomes to randomly choose and compare in
+//standard tournament selection
+#define NUM_TO_COMPARE  5
+//maximum amount of generations the ga is allowed to run before
+//restarting
+#define MAX_GENERATIONS_ALLOWED 500
 
 
 
