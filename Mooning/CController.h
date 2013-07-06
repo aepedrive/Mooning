32f2777e518f2622f307bd7c9437405848d5ebc9
aepedrive <armando.pedraza1979@gmail.com>
2013-07-06 15:16:56 -0600

    Modify CController.h file for GA operation

--- Mooning/CController.h
+++ Mooning/CController.h
@@ -1,14 +1,11 @@
 #ifndef CCONTROLLER_H
 #define CCONTROLLER_H
-//-----------------------------------------------------------------------
-//
-//  Name: CLander.h
-//
-//  Author: Mat Buckland 2002
-//
-//	Desc: Controller class for the lunar lander project.
-//
-//-----------------------------------------------------------------------
+/*! \file CController.h
+*
+*  \author Mat Buckland 2002
+*
+*	\brief Controller class for the lunar lander project.
+*/
 
 #include <windows.h>
 #include <vector>
@@ -16,17 +13,28 @@
 
 #include "utils.h"
 #include "CLander.h"
-
+#include "CgaLander.h"
 
 using namespace std;
 
-
+/// Class declaration
 class CController 
 {
 private:
 
-  //this is the lander the user can control
-  CLander*        m_pUserLander;
+	//pointer to the genetic algorithm
+	CgaLander* m_pGA;
+
+	vector<CLander> m_vecLanders;
+
+	//stores all out genomes
+	vector<SGenome> m_vecPop;
+
+	//toggles the speed at which the simulation runs
+	bool m_bFastRender;
+
+	//if switched on shows best attempt each generation
+	bool m_bShowFittest;
 
   //true if we have successfully landed
   bool            m_bSuccess;
@@ -40,6 +48,12 @@ private:
   //position of the landing pad
   SVector2D       m_vPadPos;
 
+  //generation counter
+  int m_iGeneration;
+
+  //keeps a track of the fittest lander each generation
+  int m_iFittest;
+
   //keeps a record of the window size
   int             m_cxClient,
                   m_cyClient;
@@ -48,12 +62,18 @@ private:
 
   void RenderLandingPad(HDC &surface);
 
+  void Success();
+
 
 public:
   
   CController(int cxClient, int cyClient);
 
-  ~CController();
+  ~CController()
+  {
+	  if(m_pGA)
+		  delete m_pGA;
+  }
 
   //this is called from the windows message loop and calls
   //CLander::CheckForKeyPress() which tests for user input 
@@ -66,6 +86,14 @@ public:
   //this is called from WM_PAINT to render all the objects
   //in our scene
   void  Render(HDC &surface);
+
+  void ToggleFastRender() {m_bFastRender = !m_bFastRender;}
+
+  void ToggleShowFittest();
+
+  ///accesor method
+  bool FastRender()const {return m_bFastRender;}
+  bool ShowFittest()const {return m_bShowFittest;}
   
 };
 
